a75a159f8a960a3c2f43033f8be766a8490fbec2
Michael Mills <tubas.unite@gmail.com>
2020-09-29 22:59:43 -0500

    Final changes - Rename lambda function to be more descriptive, hook up plugin to execute lambda post stack update, created function to grab sample data and store to created s3 bucket

--- .serverless_plugins/invoke-lambda-dataload.js
+++ .serverless_plugins/invoke-lambda-dataload.js
@@ -1,27 +1,49 @@
 'use strict';
+const aws = require('aws-sdk');
 
 class InvokeLambdaLoad {
-  constructor() {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
     this.commands = {
       deploy: {
-        lifecycleEvents: ['resources', 'displayStackOutputs'],
+         lifecycleEvents: ['displayStackOutputs'],
       },
     };
 
     this.hooks = {
-      'after:deploy:resources': this.afterDeployResources,
-      'after:aws:info:displayStackOutputs': this.afterDisplayStackOutputs
+      'after:aws:info:displayStackOutputs': this.afterDisplayStackOutputs.bind(this)
     };
   }
 
-  afterDeployResources() {
-    console.log('After Deploy Resources');
-  }
-
   afterDisplayStackOutputs() {
-    console.log('After Display Stack Outputs');
-    // TODO execute lambda
-    // TODO maybe push sampleData to S3?
+    console.log('Executing lambda data load');
+
+    aws.config.update({
+      region: this.serverless.service.provider.region
+    })
+    var lambda = new aws.Lambda();
+
+    var stage = this.options.stage ? this.options.stage : this.serverless.service.provider.stage;
+
+    var invokeParams = {
+      FunctionName: this.serverless.service.functions.loadSampleDataToDynamo.name,
+      Payload: JSON.stringify({
+        "BucketName": `near-earth-asteroids-${stage}`,
+        "FileName": "sampleData.json",
+        "TableName": `NearEarthAsteroids_${stage}`
+      })
+    }
+    lambda.invoke(invokeParams, function(err, data) {
+      if(err) {
+        console.log(err);
+        console.log(err.stack);
+        console.log("Error encountered, please review");
+      } else {
+        console.log(data);
+        console.log("Lambda data load successful!!!!!!");
+      }
+    })
   }
 }
 
